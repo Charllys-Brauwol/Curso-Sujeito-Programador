ce4dcb76978158b8fd1db2532d390bab2b823441
Charllys <charllysbrauwolhaha@gmail.com>
2023-07-13 09:14:41 -0300

    JS básico

---
+++ scriptescrevendopag.js
@@ -0,0 +1,6 @@
+var nome = prompt("Qual seu nome?");
+var sobrenome = prompt("Qual seu sobrenome?");
+
+console.log("Olá " + nome + sobrenome);
+
+document.write("<h1>Bem vindo " + nome + sobrenome + "</h1>");
