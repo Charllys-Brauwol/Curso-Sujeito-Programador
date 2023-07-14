bd033be53f9b8c8278bdcef6f6ede614147ba2ad
Charllys <charllysbrauwolhaha@gmail.com>
2023-07-14 10:22:37 -0300

    JS básico - Loops(While e For)

---
+++ JS Básico/scriptloops.js
@@ -0,0 +1,15 @@
+//while = enquanto;
+
+/*var x = 0;
+
+while(x <10){
+    document.write("<br> O valor de x é: " + x);
+    x++;
+}
+
+// for = para
+
+for(a = 0; a < 5; a++){
+    document.write("<br> O valor de a é: " + a); 
+}*/
+
