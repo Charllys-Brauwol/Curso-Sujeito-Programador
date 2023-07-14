418c143e240d3cbd803b37b99261099b15a0eddf
Charllys <charllysbrauwolhaha@gmail.com>
2023-07-14 10:34:50 -0300

    JS básico - Loops(While e For) e Switch

--- JS Básico/scriptloops.js
+++ JS Básico/scriptloops.js
@@ -13,3 +13,26 @@ for(a = 0; a < 5; a++){
     document.write("<br> O valor de a é: " + a); 
 }*/
 
+//switch = condicional
+
+function pedir() {
+    var valor = prompt("Digite um valor de 1 a 4");
+
+    switch (Number(valor)) {
+
+        case 1:
+            alert("Você escolheu suco");
+            break;
+        case 2:
+            alert("Você escolheu água");
+            break;
+        case 3:
+            alert("Você escolheu sorvete.");
+            break;
+        case 4:
+            alert("Você chamou o garçom.");
+            break;
+        default:
+            alert("Escolha uma opção.")
+    }
+}
