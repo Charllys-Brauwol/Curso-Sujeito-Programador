825fd0dde882c61967de7cd57f73b30ef0cd5a43
Charllys <charllysbrauwolhaha@gmail.com>
2023-07-17 18:08:34 -0300

    JS Avançando - Objetos

---
+++ JS Avançando/scriptobj.js
@@ -0,0 +1,23 @@
+/*let pessoa = {
+    nome: "Charllys",
+    idade: 26,
+    altura: 1.85, 
+    cargo: "Programador"
+};
+
+let carro = {
+    nome: "Gol",
+    cor: "Branco",
+    potencia: "140cv"
+};
+
+console.log(pessoa);
+console.log(carro);*/
+
+let usuarios = [
+    {nome: "Charllys", cargo: "Programador", status: "Ativo"},
+    {nome: "Maria", cargo: "Backend", status: "ativo"},
+    {nome: "José", cargo: "RH", status: "ativo"}
+];
+
+console.log(usuarios);
