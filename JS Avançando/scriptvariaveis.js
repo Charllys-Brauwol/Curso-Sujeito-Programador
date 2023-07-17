d3952a39bdcdc217dc989a1e63f0ac9bef230001
Charllys <charllysbrauwolhaha@gmail.com>
2023-07-17 17:08:17 -0300

    Diferença entre VAR, LET e CONST

---
+++ JS Avançando/scriptvariaveis.js
@@ -0,0 +1,7 @@
+//Diferença entre var, let e const
+
+//var - pode acessar fora do escolpo e alterar
+
+//let - só acessa e altera dentro do escolpo que foi criado 
+
+//const - acessado somente no escolpo e não consegue ser alterado
