0ed9f69fb2b39faba4b0b56a321fed2989a5210f
Charllys <charllysbrauwolhaha@gmail.com>
2023-07-19 14:34:11 -0300

    JS - Evoluindo com ES6+ - Includes, StartsWith e EndsWith

---
+++ JS - Evoluindo com ES6+/scriptInclStartEnd.js
@@ -0,0 +1,16 @@
+//Includes, ver se tem algo, devolve true ou false e é case sensitive
+/*let nomes = ["Charllys", "Pedro", "Lucas"];
+console.log(nomes.includes("Pedro"));
+if(nomes.includes("Charllys")){
+    console.log(`está na lista.`)
+} else {
+    console.log(`Não está na lista.`)
+}*/
+
+//StartsWith, verifica se começa com algo, retorna true ou false
+let nomes2 = "Charllys";
+console.log(nomes2.startsWith("Ch"));
+
+//EndsWith, verifica se termina com algo, retorna true ou false
+let nomes3 = "Charllys";
+console.log(nomes2.endsWith("ys"));
