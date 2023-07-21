3fc6757aa421530b4310140ac10b5935f487231e
Charllys <charllysbrauwolhaha@gmail.com>
2023-07-21 18:45:34 -0300

    Módulo 06 - Estrutura Básica React Native

--- meuapp/App.js
+++ meuapp/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-function App(){
-  return(
+function App() {
+  return (
     <View>
       <Text>Olá Mundo!!!!</Text>
-      
+
     </View>
 
-  );  
+  );
 }
 
 export default App;
