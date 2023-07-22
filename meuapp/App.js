ee128e264bb487ddc1903163befa81d193effadf
Charllys <charllysbrauwolhaha@gmail.com>
2023-07-22 09:42:47 -0300

    Módulo 06 - Criando seus Componentes

--- meuapp/App.js
+++ meuapp/App.js
@@ -3,27 +3,31 @@ import { View, Text, Image } from "react-native";
 
 function App() {
 
-  let nome = 'Charllys';
-  let img = 'https://sujeitoprogramador.com/reactlogo.png';
-
   return (
     <View>
       <Text>Olá Mundo!</Text>
       <Text style={{ color: 'red', fontSize: 25, margin: 15 }}>Meu primeiro App!</Text>
-      <Text style={{ fontSize: 18, color: 'green'}}>
+      <Text style={{ fontSize: 18, color: 'green' }}>
         Sujeito Programador
       </Text>
 
-      <Image
-      source={{ uri: img }}
-      style={{ width: 300, height: 300}}
-      />
+      <Logo largura={350} altura={350} fulano="Brauwol"/>
 
-      <Text style={{ fontSize: 19 }}>
-        {nome}
-      </Text>
     </View>
   );
 }
 
 export default App;
+
+//Componente a primeira letra é maiúscula 
+function Logo(props) {
+  
+  let img = 'https://sujeitoprogramador.com/reactlogo.png';
+  
+  return (
+    <View>
+  <Image source={{ uri: img }} style={{ width: props.largura, height: props.altura }} />
+  <Text> { props.fulano } </Text>
+  </View>
+);
+}
