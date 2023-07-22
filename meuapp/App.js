6cd66607e59709494b69a6003ce11f510de7aa14
Charllys <charllysbrauwolhaha@gmail.com>
2023-07-22 15:14:34 -0300

    Módulo 06 - Flexbox e Alinhamentos

--- meuapp/App.js
+++ meuapp/App.js
@@ -4,11 +4,11 @@ import { View, Text } from "react-native";
 function App() {
 
   return (
-    <View style={{ flex: 1, backgroundColor: '#121212' }}>
+    <View style={{ flex: 1, flexDirection: 'row', alignItems:'flex-start' }}>
 
-      <View style={{ height: 65, backgroundColor: '#121212' }}></View>
-      <View style={{ flex: 1, backgroundColor: '#ddd' }}></View>
-      <View style={{ height: 65, backgroundColor: '#121212' }}></View>
+      <View style={{ height: 50, width: 50, backgroundColor: '#121212' }}></View>
+      <View style={{ height: 50, width: 50, backgroundColor: 'red' }}></View>
+      <View style={{ height: 50, width: 50, backgroundColor: 'green' }}></View>
 
     </View>
   );
