753613e8e97670e54d88ab679ce8f627032c7fa0
Charllys <charllysbrauwolhaha@gmail.com>
2023-07-22 09:31:02 -0300

    Módulo 06 - Entendendo as Props

--- meuapp/App.js
+++ meuapp/App.js
@@ -1,15 +1,29 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React from "react";
+import { View, Text, Image } from "react-native";
 
 function App() {
+
+  let nome = 'Charllys';
+  let img = 'https://sujeitoprogramador.com/reactlogo.png';
+
   return (
     <View>
-      <Text>Olá Mundo!!!!</Text>
+      <Text>Olá Mundo!</Text>
+      <Text style={{ color: 'red', fontSize: 25, margin: 15 }}>Meu primeiro App!</Text>
+      <Text style={{ fontSize: 18, color: 'green'}}>
+        Sujeito Programador
+      </Text>
 
+      <Image
+      source={{ uri: img }}
+      style={{ width: 300, height: 300}}
+      />
+
+      <Text style={{ fontSize: 19 }}>
+        {nome}
+      </Text>
     </View>
-
   );
 }
 
 export default App;
-
