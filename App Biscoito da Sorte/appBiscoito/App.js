c3f3585cfffc9e2afd7329cf394ab307406af625
Charllys <charllysbrauwolhaha@gmail.com>
2023-07-26 10:12:20 -0300

    Módulo 6 - App Biscoito da Sorte

--- App Biscoito da Sorte/appBiscoito/App.js
+++ App Biscoito da Sorte/appBiscoito/App.js
@@ -4,6 +4,37 @@ import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
 function App() {
 
   const [img, setImg] = useState(require('./src/biscoito.png'));
+  const [textoFrase, setTextoFrase] = useState('');
+
+  let frases = [
+    'A vida trará coisas boas se tiver paciência.',
+    'Demonstre amor e alegria em todas as oportunidades e verá que a paz nasce dentro de si.',
+    'Não compense na ira o que lhe falta na razão.',
+    'Defeitos e virtudes são apenas dois lados da mesma moeda.',
+    'A maior de todas as torres começa no solo.',
+    'Não há que ser forte. Há que ser flexível.',
+    'Todos os dias organiza os seus cabelos, por que não faz o mesmo com o coração?',
+    'Há três coisas que jamais voltam; a flecha lançada, a palavra dita e a oportunidade perdida.',
+    'A juventude não é uma época da vida, é um estado de espírito.',
+    'Podemos escolher o que semear, mas somos obrigados a colher o que plantamos.',
+    'Dê toda a atenção á formação dos seus filhos, sobretudo com bons exemplos da sua própria vida.',
+    'Siga os bons e aprenda com eles.',
+    'Não importa o tamanho da montanha, ela não pode tapar o sol.',
+    'O bom-senso vale mais do que muito conhecimento.',
+    'Quem quer colher rosas tem de estar preparado para suportar os espinhos.'
+  ];
+
+  function quebraBiscoito() {
+    let numeroAleatorio = Math.floor(Math.random() * frases.length)
+    
+    setTextoFrase(' "' + frases[numeroAleatorio] +'" ');
+    setImg(require('./src/biscoitoAberto.png'));
+  }
+
+  function reiniciarBiscoito() {
+    setTextoFrase('');
+    setImg(require('./src/biscoito.png'));
+  }
 
   return (
     <View style={styles.container}>
@@ -11,15 +42,15 @@ function App() {
         style={styles.img}
       />
 
-      <Text style={styles.textofrase}>Está é minha primeira frase do biscoito!</Text>
+      <Text style={styles.textofrase}> {textoFrase} </Text>
 
-      <TouchableOpacity style={styles.botao} onPress={() => {alert('OK')}}>
+      <TouchableOpacity style={styles.botao} onPress={quebraBiscoito}>
         <View style={styles.btnArea}>
           <Text style={styles.btnTexto}>Quebrar Biscoito</Text>
         </View>
       </TouchableOpacity>
 
-      <TouchableOpacity style={[styles.botao, { marginTop: 15, borderColor: '#121212' }]} onPress={() => {alert('OK')}}>
+      <TouchableOpacity style={[styles.botao, { marginTop: 15, borderColor: '#121212' }]} onPress={ reiniciarBiscoito }>
         <View style={styles.btnArea}>
           <Text style={[styles.btnTexto, { color: '#121212' }]}>Reiniciar Biscoito</Text>
         </View>
@@ -38,14 +69,14 @@ const styles = StyleSheet.create({
   img: {
     width: 230,
     height: 230,
-  }, 
+  },
   textofrase: {
-    fontSize: 20, 
+    fontSize: 20,
     color: '#dd7b22',
     margin: 30,
     fontStyle: 'italic',
     textAlign: 'center'
-  }, 
+  },
   botao: {
     width: 230,
     height: 50,
