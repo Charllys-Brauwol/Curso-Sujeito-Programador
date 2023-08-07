752aed7cf17e0e1be56a5f403cdf8a9f91fe8f78
Charllys <charllysbrauwolhaha@gmail.com>
2023-08-07 18:34:32 -0300

    Módulo 07 - InstaSujeito

--- Modulo 07 InstaSujeito/instaSujeito/App.js
+++ Modulo 07 InstaSujeito/instaSujeito/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { View, Text, StyleSheet } from 'react-native';
 
+import Header from "./src/Header";
+
 export default function App() {
   return (
     <View style={styles.container}>
-
+      <Header></Header>
 
     </View>
   );
---
+++ Modulo 07 InstaSujeito/instaSujeito/src/Header/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function Header() {
+    return (
+        <View style={styles.header}>
+            <Text>Meu Header</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    header: {
+        flex: 1,
+    },
+})
