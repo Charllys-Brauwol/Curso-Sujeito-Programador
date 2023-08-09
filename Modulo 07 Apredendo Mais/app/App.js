67ffb302adb6b51a589afcf4b3f30c2dc25f331e
Charllys <charllysbrauwolhaha@gmail.com>
2023-08-09 17:51:00 -0300

    Módulo 07 - useEffect e AsyncStorage

--- Modulo 07 Apredendo Mais/app/App.js
+++ Modulo 07 Apredendo Mais/app/App.js
@@ -1,20 +1,49 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from "react-native";
+
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function App() {
-  const [contador, setContador] = useState(0);
+  const [input, setInput] = useState('');
+  const [nome, setNome] = useState('');
 
   useEffect(() => {
-    console.log('Montou');
+    async function loadData() {
+      await AsyncStorage.getItem('@nome').then((value) => {
+        setNome(value)
+      })
+    }
 
-    return () => console.log('Desmontado');
+    loadData();
+
   }, []);
+
+  async function gravaNome() {
+    await AsyncStorage.setItem('@nome', input)
+    setNome(input);
+
+    setInput('');
+  }
+
 
   return (
     <View style={styles.container}>
-      <Button title="Aumentar" onPress={() => setContador(contador + 1)} />
-      <Text style={{ fontSize: 30 }}>{contador}</Text>
-      <Button title="Diminuir" onPress={() => setContador(contador - 1)} />
+
+      <View style={styles.viewInput}>
+        <TextInput
+          style={styles.input}
+          value={input}
+          onChangeText={(texto) => setInput(texto)}
+        />
+
+        <TouchableOpacity onPress={gravaNome}>
+          <Text style={styles.btn}>+</Text>
+        </TouchableOpacity>
+
+      </View>
+
+      <Text style={styles.nome}>{nome}</Text>
+
     </View>
   );
 }
@@ -22,7 +51,29 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 35
+  },
+  viewInput: {
+    flexDirection: 'row',
     alignItems: 'center'
   },
+  input: {
+    width: 350,
+    height: 40,
+    borderColor: '#000',
+    borderWidth: 1,
+    padding: 10
+  },
+  btn: {
+    backgroundColor: '#222',
+    color: '#fff',
+    height: 40,
+    padding: 10,
+    marginLeft: 4
+  },
+  nome: {
+    marginTop: 15,
+    fontSize: 30
+  }
 })
