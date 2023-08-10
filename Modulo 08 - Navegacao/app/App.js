1175a59bda8cd0a8b23c39e315783ce8c78ff3fb
Charllys <charllysbrauwolhaha@gmail.com>
2023-08-10 17:07:10 -0300

    Módulo 08 - Icons

--- Modulo 08 - Navegacao/app/App.js
+++ Modulo 08 - Navegacao/app/App.js
@@ -1,10 +1,20 @@
 import React from "react";
 import { View, Text, StyleSheet } from 'react-native';
 
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+
 export default function App() {
   return (
     <View style={styles.container}>
+
       <Text>Sujeito Programador</Text>
+
+      <FontAwesome
+        name={"home"}
+        size={25}
+        color="#11118c"
+      />
+
     </View>
   );
 }
