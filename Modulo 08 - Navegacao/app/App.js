8213a4f22f19f4d9f591cd16446f16cd4aed588c
Charllys <charllysbrauwolhaha@gmail.com>
2023-08-10 18:03:12 -0300

    Módulo 08 - Introdução a Navegação

--- Modulo 08 - Navegacao/app/App.js
+++ Modulo 08 - Navegacao/app/App.js
@@ -1,44 +1,13 @@
 import React from "react";
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
-import FontAwesome from 'react-native-vector-icons/FontAwesome';
-import Feather from 'react-native-vector-icons/Feather'
 
 export default function App() {
+
   return (
     <View style={styles.container}>
 
       <Text>Sujeito Programador</Text>
-
-      <FontAwesome
-        name={"home"}
-        size={35}
-        color="#11118c"
-      />
-
-      <FontAwesome
-        name={"user"}
-        size={25}
-        color="#54a300"
-      />
-
-      <Feather
-        name={"gift"}
-        size={65}
-        color="#7665ff"
-      />
-
-      <TouchableOpacity style={styles.btnYoutube}>
-
-        <FontAwesome
-          name={"youtube"}
-          size={25}
-          color="#fff"
-        />
-
-        <Text style={styles.btnText}>Acessar Canal</Text>
-
-      </TouchableOpacity>
 
     </View>
   );
@@ -50,16 +19,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center'
   },
-  btnYoutube:{
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 5,
-    backgroundColor: '#ff0000',
-    borderRadius: 5
-  },
-  btnText: {
-    paddingLeft: 10,
-    color: '#fff'
-  }
 })
