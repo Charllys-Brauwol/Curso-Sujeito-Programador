ef5062b5089247410ff9e8755312fe75992a528c
Charllys <charllysbrauwolhaha@gmail.com>
2024-10-18 19:25:47 -0300

    Aula 05 - Escrevendo na página

---
+++ Módulo 02 - JS Básico/Aula 05 - Escrevendo na página/script.js
@@ -0,0 +1,6 @@
+var nome = prompt("Qual o seu nome?");
+var sobrenome = prompt("Seu sobrenome?");
+
+console.log("Olá " + nome + " " + sobrenome);
+
+document.write("<h1>Bem-vindo " + nome + " " + sobrenome + "</h1>");
