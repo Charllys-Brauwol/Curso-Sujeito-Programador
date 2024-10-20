89d947200baab888e69cc866a46b7760a1484c71
Charllys <charllysbrauwolhaha@gmail.com>
2024-10-20 14:24:45 -0300

    Aula 13 - Apredendo Loops

---
+++ Módulo 02 - JS Básico/Aula 13 - Aprendendo Loops/script.js
@@ -0,0 +1,18 @@
+//while = ENQUANTO
+/*
+var x = 0;
+
+while (x < 10) {
+    document.write("<br> O valor de X é: " + x);
+    x++;
+}
+*/
+
+//for = PARA
+/*
+for(a = 0; a < 5; a++){
+    document.write("<br> O valor de A é: " + a);
+}
+*/
+
+//
